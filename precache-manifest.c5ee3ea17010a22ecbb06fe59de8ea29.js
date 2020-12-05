138f4ecb27f18bc4507fac3149e75c857bdaea1a
Haris Ghori <hghori677@gmail.com>
2020-12-05 11:36:37 +0500

    Updates

---
+++ precache-manifest.c5ee3ea17010a22ecbb06fe59de8ea29.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14fb0e6903a8bff387d6ad41464d419e",
+    "url": "/material-table-costumized/index.html"
+  },
+  {
+    "revision": "76a41a88401ddaa86181",
+    "url": "/material-table-costumized/static/css/main.9791b2e2.chunk.css"
+  },
+  {
+    "revision": "dd4d98b87c009354419f",
+    "url": "/material-table-costumized/static/js/2.cb433234.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/material-table-costumized/static/js/2.cb433234.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70d82e54bbbaf6182299",
+    "url": "/material-table-costumized/static/js/3.af4674d6.chunk.js"
+  },
+  {
+    "revision": "a4dbed3df959bd26f81fdd1553fa8549",
+    "url": "/material-table-costumized/static/js/3.af4674d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00343ea6bb9f74e32c2c",
+    "url": "/material-table-costumized/static/js/4.1b2459d1.chunk.js"
+  },
+  {
+    "revision": "0512cf1466087f5c44c5c61e4b32a001",
+    "url": "/material-table-costumized/static/js/4.1b2459d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d27a7dc31c9ec834ea6c",
+    "url": "/material-table-costumized/static/js/5.d1400bdf.chunk.js"
+  },
+  {
+    "revision": "3a7bc1760b9d274e2e5e7531bce86076",
+    "url": "/material-table-costumized/static/js/5.d1400bdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76a41a88401ddaa86181",
+    "url": "/material-table-costumized/static/js/main.3203cb25.chunk.js"
+  },
+  {
+    "revision": "fc8eedb78363d5ca9d7c",
+    "url": "/material-table-costumized/static/js/runtime-main.292038b8.js"
+  }
+]);
